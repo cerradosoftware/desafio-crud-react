96b11747792c4188b982e0dd85d65a4e684ae316
ViniciusMaboni <viniciusmaboni@hotmail.com>
2019-12-10 23:41:32 -0300

    Ajustes layout

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,18 +1,18 @@
 import React from "react";
 import InputMask from "react-input-mask";
 
-const FormInput = ({ label, value, placeholder, onBlur, onChange, mask, addClassDiv = "", addClassInput = "" }) => {
+const FormInput = ({ label, value, placeholder, onBlur, onChange, mask, addClassDiv = "", addClassInput = "", required = false }) => {
 
     if(mask){
         return (<div className={"form-group " + addClassDiv}>
         <label >{label}</label>
-        <InputMask mask={mask}  className={"form-control " + addClassInput} value={value} onChange={onChange} onBlur={onBlur} placeholder={placeholder} />
+        <InputMask mask={mask} required={required} className={"form-control " + addClassInput} value={value} onChange={onChange} onBlur={onBlur} placeholder={placeholder} />
       </div>)
     }
 
     return (<div className={"form-group " + addClassDiv}>
         <label>{label}</label>
-        <input type="text" className={"form-control "+ addClassInput} value={value} onChange={onChange} onBlur={onBlur} placeholder={placeholder} />
+        <input type="text"  required={required} className={"form-control "+ addClassInput} value={value} onChange={onChange} onBlur={onBlur} placeholder={placeholder} />
     </div>);
 }
 
--- src/pages/NovoCliente.js
+++ src/pages/NovoCliente.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { useAuth } from "../context/auth";
+//import { useAuth } from "../context/auth";
 import axios from 'axios';
 import FormInput from "../components/FormInput";
-import  { FormSelectEstados } from "../components/FormSelect";
+import { FormSelectEstados } from "../components/FormSelect";
 
 
 const NovoCliente = () => {
-  const { authTokens } = useAuth();
+  //const { authTokens } = useAuth();
   //axios.defaults.headers.common['Authorization'] = "Bearer " + authTokens;
 
   //dados do formulario
@@ -20,49 +20,57 @@ const NovoCliente = () => {
   let [uf, setUf] = useState("");
 
   const buscaPorCep = () => {
-    axios.get(`/ws/${cep}/json/`)
-      .then((response) => {
-        console.log(response);
-        setLogradouro(response.data.logradouro);
-        setComplemento(response.data.complemento);
-        setBairro(response.data.bairro);
-        setCidade(response.data.localidade);
-        setUf(response.data.uf);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    if (cep.length === 9) {
+      axios.get(`/ws/${cep}/json/`)
+        .then((response) => {
+          console.log(response);
+          setLogradouro(response.data.logradouro);
+          setComplemento(response.data.complemento);
+          setBairro(response.data.bairro);
+          setCidade(response.data.localidade);
+          setUf(response.data.uf);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
   }
 
   return (
-    <div style={styles.container} >
-      <form>
-        <h3 className="mb-3">Dados Pessoais</h3>
-        <div className="form-row">         
-          <FormInput label="Nome" value={nome} onChange={(e) => setNome(e.target.value)} placeholder="Fulano da Silva Sauro" addClassDiv="col-md-7" />
-          <FormInput label="CPF (Somente numero)"  mask="999.999.999-99" value={cpf} onChange={(e) => setCpf(e.target.value)} placeholder="000.000.000-00" addClassDiv="col-md-5" />
+        <form style={styles.root}  className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <h3 className="mb-3">Dados Pessoais</h3>
+            <div className="form-row">
+              <FormInput label="Nome" value={nome} onChange={(e) => setNome(e.target.value)} placeholder="Fulano da Silva Sauro" addClassDiv="col-md-7" required />
+              <FormInput label="CPF (Somente numero)" mask="999.999.999-99" value={cpf} onChange={(e) => setCpf(e.target.value)} placeholder="000.000.000-00" addClassDiv="col-md-5" required />
+            </div>
+            <h3 className="mb-3">Endereço</h3>
+            <FormInput label="CEP (Somente numeros)" mask="99999-999" value={cep} onChange={(e) => setCep(e.target.value)} onBlur={buscaPorCep} placeholder="00000-000" addClassInput="col-md-3" required />
+            <FormInput label="Logradouro" value={logradouro} onChange={(e) => setLogradouro(e.target.value)} placeholder="Rua, nº" required />
+            <FormInput label="Complemento" value={complemento} onChange={(e) => setComplemento(e.target.value)} placeholder="Casa 2, ap 109" />
+            <FormInput label="Bairro" value={bairro} onChange={(e) => setBairro(e.target.value)} placeholder="Asa Norte" required />
+            <div className="form-row">
+              <FormInput label="Cidade" value={cidade} onChange={(e) => setCidade(e.target.value)} placeholder="Brasilia" addClassDiv="col-md-6" required />
+              <FormSelectEstados label="UF" value={uf} onChange={(e) => setUf(e.target.value)} addClassDiv="col-md-6" required />
+            </div>
+          </div>
+          <div className="col">
+            <h3 className="mb-3">Dados Contato</h3>
+          </div>
         </div>
-        <h3 className="mb-3">Endereço</h3>
-        <FormInput label="CEP (Somente numeros)" mask="99999-999" value={cep} onChange={(e) => setCep(e.target.value)} onBlur={buscaPorCep} placeholder="00000-000" addClassInput="col-md-3" />
-        <FormInput label="Logradouro" value={logradouro} onChange={(e) => setLogradouro(e.target.value)} placeholder="Rua, nº" />
-        <FormInput label="Complemento" value={complemento} onChange={(e) => setComplemento(e.target.value)} placeholder="Casa 2, ap 109" />
-        <FormInput label="Bairro" value={bairro} onChange={(e) => setBairro(e.target.value)} placeholder="Asa Norte" />
-        <div className="form-row">
-          <FormInput label="Cidade" value={cidade} onChange={(e) => setCidade(e.target.value)} placeholder="Brasilia" addClassDiv="col-md-6" />
-          <FormSelectEstados label="UF" value={uf}  onChange={(e) => setUf(e.target.value)} addClassDiv="col-md-6" />
-        </div>
-        <button type="submit" className="btn btn-primary">Entrar</button>
-      </form>
-    </div>
+       <button type="submit" className="btn btn-primary">Cadastrar</button>
+      </form>   
   );
 }
 
 const styles = {
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    marginTop: 20
-
+  root: {
+   
+    marginTop: 20,
+    marginLeft: 50,
+    marginRight: 50
   },
 
 }
