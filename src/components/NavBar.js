1ed5608f911a74aeaf49eb1cd853bc8e6eb5ca25
ViniciusMaboni <viniciusmaboni@hotmail.com>
2019-12-11 03:00:34 -0300

    Ajueste na listagem.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,14 +4,14 @@ import { useAuth } from "../context/auth";
 
 const Navbar = () => {
 
-  const { setAuthTokens } = useAuth();
+  const { authTokens, setAuthTokens } = useAuth();
 
   const logOut = () => {
     setAuthTokens();
   }
 
   return <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="nav-link" to="/novo">Novo Cliente</Link>
+    {(authTokens.role === "ROLE_ADMIN") && <Link className="nav-link" to="/novo">Novo Cliente</Link>}
     <Link className="nav-link" to="/">Admin Page</Link>
     <button style={styles.logOut} type="button" className="btn btn-link" onClick={logOut}>Log out</button>
   </nav>
--- src/components/TabelaClientes.js
+++ src/components/TabelaClientes.js
@@ -1,20 +1,41 @@
 import React from "react";
+import StringMask from "string-mask";
+
 export const TabelaClientes = ({ data }) => {
   return <table className="table table-striped">
     <thead>
       <tr>
         <th scope="col">#</th>
         <th scope="col">Nome</th>
-        <th scope="col">CPF</th>
-        <th scope="col">e-mail</th>
+        <th scope="col">CPF</th>        
+        <th scope="col">Logradouro</th>
+        <th scope="col">Bairro</th>
+        <th scope="col">Cidade</th>
+        <th scope="col">uf</th>
+        <th scope="col">cep</th>
+        <th scope="col">Telefone (principal)</th>
+        <th scope="col">Email (principal)</th>
       </tr>
     </thead>
     <tbody>
       {data.map((c, i) => {
+
+        let maskCpf = new StringMask('000.000.000-00').apply(c.cpf);
+        let maskCep = new StringMask('00000-000').apply(c.cep);
+        let maskFone = (c.telefones[0].tipo == "CELULAR")?new StringMask('(00) 00000-0000').apply(c.telefones[0].numero)
+        :new StringMask('(00) 00000-0000').apply(c.telefones[0].numero);
+
+
         return <tr key={i}>
           <th scope="row">{c.id}</th>
           <td>{c.nome}</td>
-          <td>{c.cpf}</td>
+          <td>{maskCpf}</td>
+          <td>{c.endereco.logradouro}</td>
+          <td>{c.endereco.bairro}</td>
+          <td>{c.endereco.cidade}</td>
+          <td>{c.endereco.uf}</td>
+          <td>{maskCep}</td>
+          <td>{c.telefones[0].tipo} - {maskFone}</td>
           <td>{c.emails[0]}</td>
         </tr>;
       })}
